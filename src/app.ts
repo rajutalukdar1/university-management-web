47e1665a990f2e60ee2facd27528a7192c9b8bbd
rajutalukdar1 <lalutaluker@gmail.com>
2023-07-02 16:04:07 +0600

    route add controler add

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
+import router from './app/modules/users/users.route'
 const app: Application = express()
 
 app.use(cors())
@@ -7,8 +8,15 @@ app.use(cors())
 //parser
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use('/api/v1/users/', router)
 
-app.get('/', (req: Request, res: Response) => {
+// testing
+app.get('/', async (req: Request, res: Response) => {
+  // await usersService.createUser({
+  //   id: '999',
+  //   password: '1234',
+  //   role: 'student',
+  // })
   res.send('Working Successfully')
 })
 
